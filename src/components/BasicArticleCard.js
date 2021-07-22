d73daded9a1c66f126e9cd36179c5b87aed36838
Tidbits JS <tidbitsjs@gmail.com>
2021-07-22 15:51:28 +0530

    basic article card

---
+++ src/components/BasicArticleCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./basicArticleCard.css";
+
+const BasicArticleCard = () => {
+  return (
+    <div className="basic-article-card__container">
+      <div className="basic-article-card__container__image-div">
+        <img
+          src="https://source.unsplash.com/1600x900/?nature"
+          alt="something"
+        />
+      </div>
+      <div className="basic-article-card__container__content-div">
+        <div className="basic-article-card__container__content-div__tag">
+          <div>Google</div>
+        </div>
+        <div className="basic-article-card__container__content-div__title">
+          <h2>Is Chromebook Still a Thing</h2>
+        </div>
+        <div className="basic-article-card__container__content-div__para">
+          <p>
+            Nunc fringilla turpis tincidunt purus malesuada, vel faucibus mauris
+            finibus.
+          </p>
+        </div>
+        <div className="basic-article-card__container__content-div__end">
+          <div className="basic-article-card__container__content-div__end-readMore">
+            <p>Read More</p>
+          </div>
+          <div className="basic-article-card__container__content-div__end-authors">
+            <div className="basic-article-card__container__content-div__end-authors__circle">
+              <img
+                src="https://source.unsplash.com/1600x900/?nature"
+                alt="author"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BasicArticleCard;
--- src/container/pages/Home/Home.js
+++ src/container/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BasicArticleCard from "../../../components/BasicArticleCard";
 import ShortcutBar from "../../../components/ShortcutBar";
 
 import "./home.css";
@@ -6,7 +7,15 @@ import "./home.css";
 const Home = () => {
   return (
     <div className="ruby-blog__home-container">
-      <div className="ruby-blog__home-container__content">conetent</div>
+      <div className="ruby-blog__home-container__content">
+        <BasicArticleCard />
+        <BasicArticleCard />
+        <BasicArticleCard />
+        <BasicArticleCard />
+        <BasicArticleCard />
+        <BasicArticleCard />
+        <BasicArticleCard />
+      </div>
       <div className="ruby-blog__home-container__sideBar">
         <ShortcutBar />
       </div>
